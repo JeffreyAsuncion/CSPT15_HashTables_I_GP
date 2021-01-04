b097f9e2af49ce6549105fb685f9d4641c4a07c3
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-01-04 00:59:43 -0500

    Updated and reviewed HashTable Scratchpad

--- src/scratchpad.py
+++ src/scratchpad.py
@@ -10,29 +10,29 @@ class SimpleHashTable:
         key_bytes = key.encode()
         # start from an arbitrary prime number (larger = better) 5381
         hash_value = 5381
-
+ 
         # bit shift and sum up the value for each char
         for byte in key_bytes:
-            hash_value = ((hash_value << 5) + hash_value) + byte
+            # TO HELP AVOID COLLISIONS - SHIFTING AND MASKING
+            hash_value = ((hash_value << 5) + hash_value) + byte 
             # hash_value = 5381 << 5 + 5381 + "e"
             hash_value &= 0xfffffffff # keep the number to be only 32bit
-
 
         # return the hash value
         return hash_value
 
 
-    def basic_hash(self, string):
-        # take in some string
+    def basic_hash(self, key):
+        # take in some string/key
         # turn the string in to bytes
-        str_bytes = string.encode()
+        str_bytes = key.encode() # split up into array of bytes
 
         # set up a counter or a sum variable
         sum = 0
         # iterate over each byte
         for byte in str_bytes:
             # increment our counter / sum by the value at that byte
-            sum += byte
+            sum += byte # concatenate
 
         # pass on the value to the caller (return the sum)
         return sum
@@ -48,6 +48,7 @@ class SimpleHashTable:
 
     def remove(self, key):
         self.storage[self.hash_index(key)] = None
+
 
 ht = SimpleHashTable(10)
 
